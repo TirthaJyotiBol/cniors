8fbba779fbf70919fec1e18d62617778f21fa652
Tirtha Jyoti Bol <83816482+TirthaJyotiBol@users.noreply.github.com>
2021-06-06 00:42:42 +0530

    next commit

---
+++ cniors/consult.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <style>  <?php include 'style.css' ?></style>
+    <title>Consults | Cniors</title>
+    <?php include 'links.php'?>
+
+  </head>
+  <body>
+    <div class="consultbody">
+  <div class="navigation-bar">
+<!-- <p class="navitems" id="awesomelogo"><i id="fa" class="fas fa-head-side-virus"3x></i></p>  -->
+<p class="navitems" id="home"> <a  class="navitem" href="home.php">Home</a></p><hr class="lineinsidenavigation">
+<p class="navitems"> <a  class="navitem" href="papers.php">Papers</a></p><hr class="lineinsidenavigation">
+<p class="navitems"> <a  class="navitem" href="roadmaps.php">Roadmaps</a></p><hr class="lineinsidenavigation">
+<p class="navitems"> <a  class="navitem" href="aboutus.php">AboutUs</a></p><hr class="lineinsidenavigation">
+<p class="navitems"> <a id="home-nav" class="navitem"  href="consult.php">Consult</a></p><hr class="lineinsidenavigation">
+<p class="navitems"> <a class="navitem"  href="login.php">Login</a></p>
+</div>
+
+<!-- Second part -->
+
+
+<div class="sliders-consult">
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://cdn.pixabay.com/photo/2017/07/10/23/43/question-mark-2492009__340.jpg" class="d-block w-100" alt="first">
+    </div>
+    <div class="carousel-item">
+      <img src="https://cdn.pixabay.com/photo/2019/06/28/20/42/questions-4304981__340.jpg" class="d-block w-100" alt="second">
+    </div>
+    <div class="carousel-item">
+      <img src="https://cdn.pixabay.com/photo/2016/09/05/18/26/board-1647323__340.jpg" class="d-block w-100" alt="third">
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="whatsapp">
+<h1 id="youcan"> You can Ask Questions through WhatsApp</h1> <hr>
+<span>
+<p id="cnwrite">Cniors is a students coterie build by the student of Chandigarh Group of Colleges,Landran
+.  This site provides you  all the
+ necessities of the students which include question papers of MST , PRE-MST, PTU 
+for all the streams ,Along with this 
+we provide various roadmaps which on following can gradually build your skills 
+for resume ,also you can consult the seniors from the  college. <br>
+Below is the whatsapp button given , if you want to ask question through WhatsApp Contact us.
+ </p>
+ 
+ <img id="imge" src="https://cdn.pixabay.com/photo/2017/10/14/09/56/journal-2850091__340.jpg" alt="image">
+ </span>
+ <div class="none"> 
+     <a target="_blank" href="https://wa.me/916003010876" title="click here to chat in whatsapp"> <p>WhatsApp</p> </a>
+ </div>
+
+ <div class="whatsappbutton"> 
+   <!-- //WhatsApp click to chat is used. The link here is taken form whatsApp web -->
+     <a target="_blank" href="https://wa.me/916003010876" title="click here to chat in whatsapp"> <p>WhatsApp</p> </a>
+ </div>
+
+
+</div>
+
+
+
+                                                        <!-- FOOTER -->
+
+                                                        <section class="footer">
+<div class="contacts">
+    <p id="contactus" class="footeroneitems">ContactUs</p> <br>
+    <p id="email" class="contactusitems"><i id="emailicon" class="fas fa-envelope"></i> <a class="contactwriting"href="tjstyles555@gmail.com">Email</a></p> 
+    <p id="github" class="contactusitems"><i id="githubicon" class="fab fa-github"></i> <a class="contactwriting"href="tjstyles555@gmail.com">Git-Hub</a></p>
+    <!-- <p id="discord" class="contactusitems"> <i id="discordicon" class="fab fa-discord"></i><a class="contactwriting"href="tjstyles555@gmail.com">Discord-Server </a></p> -->
+</div>  
+<div class="about"> 
+    <p class="aboutus" ><a id="aboutus" href="aboutus.php">AboutUs</a></p> <br>
+    <p class="detailsaboutus">CNIORS is a student's coterie that helps students in many ways. <br> TO KNOW MORE SEE THE <a id="detailaboutus" href="aboutus.php">AboutUs section</a>   </p>
+</div>
+<div class="signup">
+    <p id="signup">SignUp</p> <br>
+    <p id="signupdescription">you sign into our page to get the best service  <p id="sign"><i id="idofloginicon" class="fas fa-sign-in-alt"></i><a id="signupphp" href="login.php">SIGN UP</a></p>  </p>
+</div>
+
+</section>
+
+<section class="copyright">
+    <p id=>@_all rights reserved under Cniors23@gmail.com </p>
+</section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+ 
+
+ 
+ 
+    </div>
+  </body>
+</html>
+
+
